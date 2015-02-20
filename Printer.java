e501098620fa92d52e392362429e792f62a80275
vishnumishra <vishnu143mishra@gmail.com>
2015-02-20 20:34:24 +0530

    wc working for more than one file

--- Printer.java
+++ Printer.java
@@ -1,11 +1,14 @@
 
 public class Printer{
-	public static  void printWc(WcLib wc,String path){
-		int words,lines,characters;
-		words = wc.countWords();
-		lines = wc.countLines();
-		characters = wc.countChar();
-		System.out.println("  "+lines +"	"+ words+"	" +characters +" "+path );
+	public static  void printWc(WcLib[] wcs,String[] files){
+
+		int words,lines,characters,i=0;
+		for(WcLib wc:wcs){
+			words = wc.countWords();
+			lines = wc.countLines();
+			characters = wc.countChar();
+			System.out.println("  "+lines +"	"+ words+"	" +characters +" "+files[i++] );
+		}
 	}
 }
 
--- Wc.java
+++ Wc.java
@@ -1,10 +1,14 @@
 
 public class Wc{
 	public static void main(String[] args){
-	
-		String content = MyFile.readFile(args[0]);
-		WcLib wc = new WcLib(content);
+		String[] files,flags;
+		ArgsSeprator argsSep = new ArgsSeprator(args);
+		files = argsSep.files;
+		flags = argsSep.flags;
+		
+		HandleArgs handleArgs = new HandleArgs(files);
+		WcLib[] wcObj = handleArgs.wcObj;
 
-		Printer.printWc(wc,args[0]);
+		Printer.printWc(wcObj,files);
 	}
  }
